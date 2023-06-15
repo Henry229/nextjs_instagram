e31fb7ccb00be4be7efaaa94f944f4ba785ed703
Henry Chun <chunhyungjoon@gmail.com>
2023-06-15 23:25:16 +1000

    fix & just follow lecture and add profile picture

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,28 @@
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { getProviders } from 'next-auth/react';
+import SignIn from '@/components/SignIn';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+export default async function SignInPage({
+  searchParams: { callbackUrl },
+}: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+  return (
+    <section className='flex justify-center mt-24'>
+      <SignIn providers={providers} callbackUrl={callbackUrl ?? '/'} />
+    </section>
+  );
+}
